98b79f501e70b727b1eda36bc6b804320b9f29e3
Doulrion <dulrion.info@gmail.com>
2026-01-20 22:05:05 +0100

    first commit

---
+++ src/main/java/com/doulrion/rima/RimaClient.java
@@ -0,0 +1,11 @@
+package com.doulrion.rima;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RimaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/doulrion/rima/RimaDataGenerator.java
@@ -0,0 +1,11 @@
+package com.doulrion.rima;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RimaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
