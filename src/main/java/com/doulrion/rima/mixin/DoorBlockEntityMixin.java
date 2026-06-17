2ebead0053af75f12a8788856fe2b9d36fb557c6
InherentlyBadNoodles <inherentlybadnoodles@gmail.com>
2026-06-17 10:56:45 +0200

    dynamically add blockentity support

--- src/main/java/com/doulrion/rima/mixin/DoorBlockEntityMixin.java
+++ src/main/java/com/doulrion/rima/mixin/DoorBlockEntityMixin.java
@@ -1,6 +1,7 @@
 // com/doulrion/rima/mixin/DoorBlockEntityMixin.java
 package com.doulrion.rima.mixin;
 
+import com.doulrion.rima.Rima;
 import com.doulrion.rima.blockentity.LockedDoorBlockEntity;
 import com.doulrion.rima.blockentity.RimaBlockEntityTypes;
 import net.minecraft.block.BlockState;
@@ -18,13 +19,17 @@ import org.spongepowered.asm.mixin.Interface;
 public abstract class DoorBlockEntityMixin {
 
     private static final String[] blacklist = new String[] {
-        "yuushya"
+        // "yuushya"
     };
     // Only attach a block entity to the lower half to avoid doubling
     public BlockEntity rima$createBlockEntity(BlockPos pos, BlockState state) {
         if ( ! isOnBlacklist( state.getBlock().getName().toString() )
             && state.contains(Properties.DOUBLE_BLOCK_HALF) 
             && state.get(Properties.DOUBLE_BLOCK_HALF) == net.minecraft.block.enums.DoubleBlockHalf.LOWER) {
+            if (!RimaBlockEntityTypes.LOCKED_DOOR.supports(state)){
+              RimaBlockEntityTypes.LOCKED_DOOR.addSupportedBlock(state.getBlock());     // dynamically add support for blocks
+              Rima.LOGGER.info("added DoorBlockEntity support for block: " + state.getBlock().getName().toString());
+            }
             return new LockedDoorBlockEntity(RimaBlockEntityTypes.LOCKED_DOOR, pos, state);
         }
         return null;
